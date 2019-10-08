8bdfdda7765dbcb9db46677704cf022dd2ed1323
Francis Tsang <frtsang40@gmail.com>
2019-10-07 22:19:33 -0700

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -165,8 +165,13 @@ class Simulation(object):
         # TODO: Call this method at the end of every time step and infect each Person.
         # TODO: Once you have iterated through the entire list of self.newly_infected, remember
         # to reset self.newly_infected back to an empty list.
-        pass
-
+        for person in self.population:
+            for id in self.newly_infected:
+                if person._id == id:
+                    person.infection = self.virus
+                    self.total_infected += 1
+                    self.current_infected += 1
+        self.newly_infected = []
 
 if __name__ == "__main__":
     params = sys.argv[1:]
