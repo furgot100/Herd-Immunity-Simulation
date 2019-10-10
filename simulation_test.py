d7fb09784fb05c0840281726ba0ebf365b35e8fc
Francis Tsang <frtsang40@gmail.com>
2019-10-10 14:17:38 -0700

    Update simulation_test.py

---
+++ simulation_test.py
@@ -0,0 +1,16 @@
+from simulation import Simulation
+from virus import Virus
+from person import Person
+
+def test_create_pop():
+    anthrax = Virus("Anthrax", 6.5, 0.2)
+    anthrax_sim = Simulation(100,20,40,anthrax)
+    initial_infected = (anthrax.repro_rate * 100) - 1
+    anthrax_sim._create_population(initial_infected)
+
+def test_infection():
+    anthrax = ("Anthrax", 6.5, 0.2)
+    anthrax_sim = Simulation(100,20,40, anthrax)
+    infected_person = Person(id,True,False,anthrax)
+    uninfected = Person(id,True,False, None)
+    anthrax_sim.interaction(infected_person,uninfected)
